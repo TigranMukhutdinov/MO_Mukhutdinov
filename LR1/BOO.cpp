67079d993d38b57a83779b00c731e63eac278448
TigranMukhutdinov <101561896+TigranMukhutdinov@users.noreply.github.com>
2025-12-05 10:39:38 +0500

    Обновление работы методов

--- LR1/BOO.cpp
+++ LR1/BOO.cpp
@@ -12,62 +12,85 @@ double DOP(double e) {
     a = 0;
     b = 2;
     double N = 1;
+
+    x2 = (a + b) / 2;
+    y2 = func(x2);
+
     while ((b - a) > (2 * e)) {
-        x2 = (a + b) / 2;
-        y2 = func(x2);
         x1 = (a + x2) / 2;
         y1 = func(x1);
-        x3 = (b + x2) / 2;
+        x3 = (x2 + b) / 2;
         y3 = func(x3);
         N += 2;
+
         x0 = a;
         x4 = b;
+
+        int min_index = 1;
         min = y1;
-        b = x2;
-        x2 = x1;
+
         if (y2 < min) {
             min = y2;
-            a = x1;
-            b = x3;
+            min_index = 2;
         }
         if (y3 < min) {
             min = y3;
+            min_index = 3;
+        }
+
+        if (min_index == 1) {
+            a = x0;
+            b = x2;
+            x2 = x1;
+            y2 = y1;
+        }
+        else if (min_index == 2) {
+            a = x1;
+            b = x3;
+        }
+        else {
             a = x2;
             b = x4;
             x2 = x3;
+            y2 = y3;
         }
-        y2 = min;
     }
+
+
     cout << "x min = " << x2 << " y min = " << y2 << " Кол-во экспериментов = " << N << " Точность = " << e << endl;
     return 0;
 }
 
 double ZS(double e) {
     double a, b, lambda, N, x1, y1, x2, y2;
+
     lambda = (1 + sqrt(5)) / 2;
-    N = 1;
     a = 0;
     b = 2;
+    N = 2;
+
     x1 = b - (b - a) / lambda;
     x2 = a + (b - a) / lambda;
     y1 = func(x1);
     y2 = func(x2);
+
     while ((b - a) > e) {
-        if (y1 < y2) {
+        if (y1 <= y2) {
             b = x2;
             x2 = x1;
             y2 = y1;
-            x1 = b - (b - a) / lambda;
+            x1 = a + b - x2;
             y1 = func(x1);
         } else {
             a = x1;
             x1 = x2;
             y1 = y2;
-            x2 = a + (b - a) / lambda;
+            x2 = a + b - x1;
             y2 = func(x2);
         }
         N += 1;
     }
+
     cout << "x min = " << a << " y min = " << y1 << " Кол-во экспериментов = " << N << " Точность = " << e << endl;
     return 0;
 }
